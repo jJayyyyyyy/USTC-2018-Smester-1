96d79b0e71741480f08762849e452af73b83598f
FrozenMap <ljjdbd123@hotmail.com>
2018-10-14 02:05:59 +0000

    fix bug in [ int logicalShift(int x, int n) ], handle the case when n == 0

--- CSAPP/exp/exp01/datalab-handout/bits.c
+++ CSAPP/exp/exp01/datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * Lou, Xu, Chen
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -20,103 +20,103 @@
  * STEP 1: Read the following instructions carefully.
  */
 
-You will provide your solution to the Data Lab by
-editing the collection of functions in this source file.
+// You will provide your solution to the Data Lab by
+// editing the collection of functions in this source file.
 
-INTEGER CODING RULES:
+// INTEGER CODING RULES:
  
-	Replace the "return" statement in each function with one
-	or more lines of C code that implements the function. Your code 
-	must conform to the following style:
+// 	Replace the "return" statement in each function with one
+// 	or more lines of C code that implements the function. Your code 
+// 	must conform to the following style:
  
-	int Funct(arg1, arg2, ...) {
-			/* brief description of how your implementation works */
-			int var1 = Expr1;
-			...
-			int varM = ExprM;
+// 	int Funct(arg1, arg2, ...) {
+// 			/* brief description of how your implementation works */
+// 			int var1 = Expr1;
+// 			...
+// 			int varM = ExprM;
 
-			varJ = ExprJ;
-			...
-			varN = ExprN;
-			return ExprR;
-	}
+// 			varJ = ExprJ;
+// 			...
+// 			varN = ExprN;
+// 			return ExprR;
+// 	}
 
-	Each "Expr" is an expression using ONLY the following:
-	1. Integer constants 0 through 255 (0xFF), inclusive. You are
-			not allowed to use big constants such as 0xffffffff.
-	2. Function arguments and local variables (no global variables).
-	3. Unary integer operations ! ~
-	4. Binary integer operations & ^ | + << >>
+// 	Each "Expr" is an expression using ONLY the following:
+// 	1. Integer constants 0 through 255 (0xFF), inclusive. You are
+// 			not allowed to use big constants such as 0xffffffff.
+// 	2. Function arguments and local variables (no global variables).
+// 	3. Unary integer operations ! ~
+// 	4. Binary integer operations & ^ | + << >>
 		
-	Some of the problems restrict the set of allowed operators even further.
-	Each "Expr" may consist of multiple operators. You are not restricted to
-	one operator per line.
+// 	Some of the problems restrict the set of allowed operators even further.
+// 	Each "Expr" may consist of multiple operators. You are not restricted to
+// 	one operator per line.
 
-	You are expressly forbidden to:
-	1. Use any control constructs such as if, do, while, for, switch, etc.
-	2. Define or use any macros.
-	3. Define any additional functions in this file.
-	4. Call any functions.
-	5. Use any other operations, such as &&, ||, -, or ?:
-	6. Use any form of casting.
-	7. Use any data type other than int.  This implies that you
-		cannot use arrays, structs, or unions.
+// 	You are expressly forbidden to:
+// 	1. Use any control constructs such as if, do, while, for, switch, etc.
+// 	2. Define or use any macros.
+// 	3. Define any additional functions in this file.
+// 	4. Call any functions.
+// 	5. Use any other operations, such as &&, ||, -, or ?:
+// 	6. Use any form of casting.
+// 	7. Use any data type other than int.  This implies that you
+// 		cannot use arrays, structs, or unions.
 
  
-	You may assume that your machine:
-	1. Uses 2s complement, 32-bit representations of integers.
-	2. Performs right shifts arithmetically.
-	3. Has unpredictable behavior when shifting an integer by more than the word size.
+// 	You may assume that your machine:
+// 	1. Uses 2s complement, 32-bit representations of integers.
+// 	2. Performs right shifts arithmetically.
+// 	3. Has unpredictable behavior when shifting an integer by more than the word size.
 
-EXAMPLES OF ACCEPTABLE CODING STYLE:
-	/*
-	* pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
-	*/
-	int pow2plus1(int x) {
-		/* exploit ability of shifts to compute powers of 2 */
-		return (1 << x) + 1;
-	}
+// EXAMPLES OF ACCEPTABLE CODING STYLE:
+// 	/*
+// 	* pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+// 	*/
+// 	int pow2plus1(int x) {
+// 		/* exploit ability of shifts to compute powers of 2 */
+// 		return (1 << x) + 1;
+// 	}
 
-	/*
-	* pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
-	*/
-	int pow2plus4(int x) {
-		/* exploit ability of shifts to compute powers of 2 */
-		int result = (1 << x);
-		result += 4;
-		return result;
-	}
+// 	/*
+// 	* pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+// 	*/
+// 	int pow2plus4(int x) {
+// 		/* exploit ability of shifts to compute powers of 2 */
+// 		int result = (1 << x);
+// 		result += 4;
+// 		return result;
+// 	}
 
-FLOATING POINT CODING RULES
+// FLOATING POINT CODING RULES
 
-For the problems that require you to implent floating-point operations,
-the coding rules are less strict.  You are allowed to use looping and
-conditional control.  You are allowed to use both ints and unsigneds.
-You can use arbitrary integer and unsigned constants.
+// For the problems that require you to implent floating-point operations,
+// the coding rules are less strict.  You are allowed to use looping and
+// conditional control.  You are allowed to use both ints and unsigneds.
+// You can use arbitrary integer and unsigned constants.
 
-You are expressly forbidden to:
-	1. Define or use any macros.
-	2. Define any additional functions in this file.
-	3. Call any functions.
-	4. Use any form of casting.
-	5. Use any data type other than int or unsigned.  This means that you
-		cannot use arrays, structs, or unions.
-	6. Use any floating point data types, operations, or constants.
+// You are expressly forbidden to:
+// 	1. Define or use any macros.
+// 	2. Define any additional functions in this file.
+// 	3. Call any functions.
+// 	4. Use any form of casting.
+// 	5. Use any data type other than int or unsigned.  This means that you
+// 		cannot use arrays, structs, or unions.
+// 	6. Use any floating point data types, operations, or constants.
 
 
-NOTES:
-	1. Use the dlc (data lab checker) compiler (described in the handout) to 
-		check the legality of your solutions.
-	2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
-		that you are allowed to use for your implementation of the function. 
-		The max operator count is checked by dlc. Note that '=' is not 
-		counted; you may use as many of these as you want without penalty.
-	3. Use the btest test harness to check your functions for correctness.
-	4. Use the BDD checker to formally verify your functions
-	5. The maximum number of ops for each function is given in the
-		header comment for each function. If there are any inconsistencies 
-		between the maximum ops in the writeup and in this file, consider
-		this file the authoritative source.
+// NOTES:
+// 	1. Use the dlc (data lab checker) compiler (described in the handout) to 
+// 		check the legality of your solutions.
+// 	2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+// 		that you are allowed to use for your implementation of the function. 
+// 		The max operator count is checked by dlc. Note that '=' is not 
+// 		counted; you may use as many of these as you want without penalty.
+// 	3. Use the btest test harness to check your functions for correctness.
+// 	4. Use the BDD checker to formally verify your functions
+// 	5. The maximum number of ops for each function is given in the
+// 		header comment for each function. If there are any inconsistencies 
+// 		between the maximum ops in the writeup and in this file, consider
+// 		this file the authoritative source.
 
 /*
  * STEP 2: Modify the following functions according the coding rules.
@@ -203,11 +203,18 @@ int getByte(int x, int n) {
  */
 int logicalShift(int x, int n) {
 	/* Create mask for n = 0 */
-	int zmask = (~n) + 1;
+	// n == 0 的时候特殊处理，因为 x<<32 似乎是个 undefined behavior, VS 和 gcc 上跑出来的结果不一样
+	int zmask = (!n);
 	/* Arithmetic shift right by n */
 	int right = x >> n;
+	
 	/* Mask off upper 1's */
-	int lmask = (~0) << (32 + zmask);
+	int lmask = (~0) << (33 + ~n);	// 33 + ~n = 32 + ~n + 1 = 32 - n
+	lmask &= ~((zmask<<31)>>31);
+	// 如果 n = 0, 那么 zmask = 1, lmask = 0 & lmask = 0
+	// 如果 n != 0, 那么 zmask = 0, lmask = 0xffff ffff & lmask
+	// 更多测试用例可见 ./btest
+	
 	right &= ~lmask;
 	return right;
 }
@@ -431,26 +438,26 @@ unsigned float_i2f(int x) {
  *   Max ops: 30
  *   Rating: 4
  */
-// unsigned float_twice(unsigned uf) {
-// 	unsigned sign = (uf >> 31) 0x01;
-// 	unsigned exp = uf>>23 & 0xFF;
-// 	unsigned frac = uf & 0x7FFFFF;
-// 	if (exp == 0) {
-// 		/* Denormalized.  Must double fraction */
-// 		frac = 2*frac;
-// 		if (frac > 0x7FFFFF) {
-// 			/* Result normalized */
-// 			frac = frac & 0x7FFFFF; /* Chop off leading bit */
-// 			exp = 1;
-// 		}
-// 	} else if (exp < 0xFF) {
-// 		/* Normalized.  Increase exponent */
-// 		exp++;
-// 		if (exp == 0xFF) {
-// 			/* Infinity */
-// 			frac = 0;
-// 		}
-// 	}
-// 	/* Infinity and NaN do not require any changes */
-// 	return (sign << 31) | (exp << 23) | frac;
-// }
+unsigned float_twice(unsigned uf) {
+	unsigned sign = (uf >> 31) & 0x01;
+	unsigned exp = uf>>23 & 0xFF;
+	unsigned frac = uf & 0x7FFFFF;
+	if (exp == 0) {
+		/* Denormalized.  Must double fraction */
+		frac = 2*frac;
+		if (frac > 0x7FFFFF) {
+			/* Result normalized */
+			frac = frac & 0x7FFFFF; /* Chop off leading bit */
+			exp = 1;
+		}
+	} else if (exp < 0xFF) {
+		/* Normalized.  Increase exponent */
+		exp++;
+		if (exp == 0xFF) {
+			/* Infinity */
+			frac = 0;
+		}
+	}
+	/* Infinity and NaN do not require any changes */
+	return (sign << 31) | (exp << 23) | frac;
+}
