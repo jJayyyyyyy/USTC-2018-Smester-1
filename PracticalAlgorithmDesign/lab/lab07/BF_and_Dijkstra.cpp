4d6e8bcf41d1461f8a217613b32495b9f6414035
FrozenMap <ljjdbd123@hotmail.com>
2018-12-10 12:45:11 +0000

    update annotation

--- PracticalAlgorithmDesign/lab/lab07/BF_and_Dijkstra.cpp
+++ PracticalAlgorithmDesign/lab/lab07/BF_and_Dijkstra.cpp
@@ -6,6 +6,7 @@
 #define INF 0x3fffffff
 using namespace std;
 
+// BF算法用到的邻接表的结点结构
 struct Node{
 	int vertex, distance;
 	Node(int v, int d){
@@ -16,31 +17,39 @@ struct Node{
 
 class BellmanFord{
 public:
+	// n 是结点总数
 	int n;
+	// graph 以邻接表形式保存了图的信息
 	vector<vector<Node> > graph;
+	// pre[i] 是结点 src 到结点 i 的最短路径上的 i 的前置结点
 	vector<vector<int> > pre;
+	// dist[i] 表示结点 src 到结点 i 的最短距离
+	// ansPath 保存了 src 到某个结点的最短路径
+	// ansPath 是各个结点复用的, 每次打印完最短路径后需要 ansPath.clear()
 	vector<int> dist, ansPath;
 
 	BellmanFord(){
 	}
 
+	// 构造函数
 	BellmanFord(int numVertex){
 		n = numVertex;
 		graph.resize(n);
 		dist.resize(n, INF);
 	}
 
+	// 初始化 graph, pre, dist
 	void init(){
 		graph.resize(n);
 		pre.resize(n);
 		dist.resize(n, INF);
 	}
 
-	// get shortest path d[] using BF algorithm
+	// 使用 Bellman-Ford 算法寻找 src 到各个结点的最短路径 dist[]
 	void doBF(int src){
-		fill(dist.begin(), dist.end(), INF);
 		dist[src] = 0;
 		// iteration = |V| - 1 = n - 1
+		// 即循环次数 = n - 1
 		// 结点编号为 [0, n-1]
 		for( int i = 0; i < n - 1; i++ ){
 			// 把每个结点当作中间结点 mid, 尝试更新 mid 能够到达的结点的 dist[]
@@ -49,6 +58,7 @@ public:
 					int nextV = node.vertex;
 					int distance = node.distance;
 					if( dist[mid] + distance < dist[nextV] ){
+						// 如果通过 mid 可以更快到达结点 i, 那么就更新 dist[i]
 						dist[nextV] = dist[mid] + distance;
 					}
 				}
@@ -56,7 +66,9 @@ public:
 		}
 	}
 
-	// @return: false is good, true is bad
+	// BF 算法无法处理含负边的环
+	// return true 说明图中有负边环
+	// return false 时可以正确处理
 	bool hasNegativeCircle(){
 		for( int mid = 0; mid < n; mid++ ){
 			for( Node node : graph[mid] ){
@@ -73,6 +85,7 @@ public:
 		return false;
 	}
 
+	// 获取 src 到 dest 的路径
 	void dfs(int src, int v){
 		if( v == src ){
 			ansPath.push_back(v);
@@ -85,6 +98,7 @@ public:
 		}
 	}
 
+	// 打印 src 到 dest 的路径
 	void getPath(int src, int dest){
 		ansPath.clear();
 		dfs(src, dest);
@@ -96,6 +110,7 @@ public:
 		}
 	}
 
+	// 打印最短距离和最短路径
 	void getPathAndDistance(int src, int dest){
 		// cout<<"Shortest distance from src "<<src<<" to each node is as below:\n";
 		cout<<"Path from "<<src<<" to "<<dest<<'\n';
@@ -109,6 +124,7 @@ public:
 		}
 	}
 
+	// 测试用例
 	void test(){
 		n = 6;
 		init();
@@ -141,23 +157,39 @@ public:
 
 class Dijkstra{
 public:
+	// 结点总数为 n, 编号范围 [0, n-1]
 	int n;
+	// vis[] 保存结点的访问状态
+	// 若 vis[i] == false 则说明结点 i 尚未被访问
 	vector<bool> vis;
+	// dist[i] 表示结点 src 到结点 i 的最短距离
+	// ansPath 保存了 src 到某个结点的最短路径
+	// ansPath 是各个结点复用的, 每次打印完最短路径后需要 ansPath.clear()
 	vector<int> dist, ansPath;
+	// graph 保存了图上各个结点直接的连接关系
 	vector<vector<int> > graph;
+	// pre[i] 是结点 src 到结点 i 的最短路径上的 i 的前置结点
 	vector<vector<int> > pre;
 
+	// 构造函数为空
 	Dijkstra(){
 	}
 
+	// 初始化
+	// 将 vis 全部设为未访问 false
+	// 将 dist 全部设为 INF, 即初始距离设为无穷大
 	void init(){
 		fill(vis.begin(), vis.end(), false);
 		fill(dist.begin(), dist.end(), INF);
 	}
 
+	// 使用 Dijkstra 算法寻找 src -> dest 的最短路径
 	void doDijkstra(int src, int dest){
+		// 先进行初始化
 		init();
+		// 将原点 src 距离设为 0
 		dist[src] = 0;
+		// 如果目的结点 dest 还未被访问, 就继续循环
 		while( vis[dest] == false ){
 			int mid = -1;
 			int minDist = INF;
@@ -168,17 +200,20 @@ public:
 					minDist = dist[i];
 				}
 			}
+			// mid 初始值为 -1 如果在遍历未访问结点后 mid 仍为 -1, 则说明目标结点不可达
 			if( mid == -1 ){
 				return;
 			}
 
-			// 设置 mid 为已访问结点
-			// 以 mid 为中间结点, 更新 mid 能够到达的结点的 dist[]
+			// 将 mid 置为已访问结点
 			vis[mid] = true;
+			// 以 mid 为中间结点, 更新 mid 能够到达的结点的 dist[]
 			for( int i = 0; i < n; i++ ){
 				if( vis[i] == false && graph[mid][i] != INF ){
 					if( dist[mid] + graph[mid][i] < dist[i] ){
+						// 如果通过 mid 可以更快到达结点 i, 那么就更新 dist[i]
 						dist[i] = dist[mid] + graph[mid][i];
+						// 并且将 i 的前置结点设为 mid
 						pre[i].clear();
 						pre[i].push_back(mid);
 					}
@@ -187,6 +222,7 @@ public:
 		}
 	}
 
+	// 获取 src 到 dest 的路径
 	void dfs(int src, int v){
 		if( v == src ){
 			ansPath.push_back(v);
@@ -199,6 +235,7 @@ public:
 		}
 	}
 
+	// 打印 src 到 dest 的路径
 	void getPath(int src, int dest){
 		ansPath.clear();
 		dfs(src, dest);
@@ -210,6 +247,7 @@ public:
 		}
 	}
 
+	// 打印最短距离和最短路径
 	void getPathAndDistance(int src, int dest){
 		cout<<"Path from "<<src<<" to "<<dest<<'\n';
 		int distance = dist[dest];
@@ -222,6 +260,7 @@ public:
 		}
 	}
 
+	// Dijkstra 算法无法处理有负边的图
 	bool hasNegativeEdge(){
 		size_t row = graph.size();
 		size_t col = graph[0].size();
@@ -235,6 +274,7 @@ public:
 		return false;
 	}
 
+	// 测试用例
 	void test(){
 		n = 6;
 		vis.resize(n, false);
